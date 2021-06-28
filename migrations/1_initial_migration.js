22d58434fb276ef8f22169daccd7a9f800f87198
Chet Zema <chet.zema@cama.io>
2021-06-28 17:02:04 -0400

    Voter proxy added; test deploy worked.

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,34 @@
-const Migrations = artifacts.require("Migrations");
+var fs = require('fs');
+
+const ZhVoterProxy = artifacts.require("ZhVoterProxy");
+const ZhToken = artifacts.require("ZhToken");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+
+  let contractList = {};
+  let systemContracts = {};
+  contractList["system"] = systemContracts;
+
+  let addContract = function (group, name, value) {
+    contractList[group][name] = value;
+    let output = JSON.stringify(contractList, null, 3);
+
+    fs.writeFileSync("contracts.json", output, err => {
+      if (err) {
+        return console.log("Error writing file: " + err);
+      }
+    });
+  };
+
+  let zhVoterProxy = "0x33A8466f48e50A10af2E9d060e76e93F1B8e98cb";
+
+  addContract("system", "voteProxy", zhVoterProxy);
+
+//  deployer.deploy(ZhVoterProxy);
+
+  deployer.deploy(ZhToken, zhVoterProxy).then(instance => {
+    zh = instance;
+    addContract("system", "zh", zh.address);
+    return ZhVoterProxy.at(zhVoterProxy);
+  });
 };
